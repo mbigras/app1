ea5f4e348abda4b145d05e1ebe984930ecf78e4d
Max Bigras <mbigras22@gmail.com>
2023-01-15 15:42:03 -0800

    Add image to response

--- app1.py
+++ app1.py
@@ -4,6 +4,7 @@ import socket
 import flask
 
 app = flask.Flask(os.environ.get("APP", "app1"))
+app.config["image"] = os.environ.get("IMAGE", "unset")
 app.config["org"] = os.environ.get("ORG", "unset")
 app.config["owner"] = os.environ.get("OWNER", "unset")
 app.config["env"] = os.environ.get("ENV", "unset")
@@ -15,6 +16,7 @@ app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 def hello():
     return flask.jsonify(
         app=app.name,
+        image=app.config["image"],
         org=app.config["org"],
         owner=app.config["owner"],
         env=app.config["env"],
